9d366ddd2ebfcfaa35c2d3d0d8c47dfcaca90475
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-08-21 23:30:23 +0300

    add font and header image to diary template

--- server/utils/printHelper.js
+++ server/utils/printHelper.js
@@ -4,6 +4,7 @@ import archiver from 'archiver';
 import temp from 'temp';
 import { getFileName, getPdfStreamFromHtml, renderEjsTemplate } from '../../common-modules/server/utils/template';
 import { getDiaryDataByGroupId } from './queryHelper';
+import constant from '../../common-modules/server/config/directory';
 
 temp.track();
 
@@ -11,9 +12,16 @@ const templatesDir = path.join(__dirname, '..', '..', 'public', 'templates');
 
 const getFilenameFromGroup = ({ klass, teacher, lesson }) => `יומן נוכחות ${klass?.name || ''}_${teacher?.name || ''}_${lesson?.name || ''}`;
 
+const addMetadataToTemplateData = async (templateData, title) => {
+    templateData.title = title;
+    templateData.font = 'data:font/truetype;base64,' + await fs.promises.readFile(path.join(constant.assetsDir, 'fonts', 'ELEGANTIBOLD.TTF'), { encoding: 'base64' });
+    templateData.img = 'data:image;base64,' + await fs.promises.readFile(path.join(constant.assetsDir, 'img', 'header.jpg'), { encoding: 'base64' });
+}
+
 export async function getDiaryStream(groupId) {
     const templatePath = path.join(templatesDir, "diary.ejs");
     const templateData = await getDiaryDataByGroupId(groupId);
+    await addMetadataToTemplateData(templateData, 'יומן נוכחות');
     const html = await renderEjsTemplate(templatePath, templateData);
     const fileStream = await getPdfStreamFromHtml(html);
     const filename = getFilenameFromGroup(templateData.group);
