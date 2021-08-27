196ca39addee7343d639186a3ab156bec14ea42a
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-08-27 17:28:30 +0300

    add export pdf

--- server/routes/student-klass.route.js
+++ server/routes/student-klass.route.js
@@ -1,15 +1,20 @@
 import * as studentKlassCtrl from '../controllers/student-klass.controller';
 import genericRoute from '../../common-modules/server/routes/generic.route';
+import { exportPdf } from '../../common-modules/server/utils/template';
 
 const router = genericRoute(studentKlassCtrl, router => {
     router.route('/get-edit-data')
         .get((req, res) => {
             studentKlassCtrl.getEditData(req, res);
         });
-        
+
     router.route('/report-by-klass-type')
         .get((req, res) => {
             studentKlassCtrl.reportByKlassType(req, res);
+        });
+    router.route('/report-by-klass-type/export-pdf')
+        .post((req, res) => {
+            exportPdf(req, res);
         });
 });
 
