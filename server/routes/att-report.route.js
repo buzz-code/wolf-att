b97a15ae54ee7effbf64a5509576dc1d344391f3
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-10-30 20:58:41 +0300

    add handle email function to att report

--- server/routes/att-report.route.js
+++ server/routes/att-report.route.js
@@ -1,10 +1,17 @@
 import * as attReportCtrl from '../controllers/att-report.controller';
 import genericRoute from '../../common-modules/server/routes/generic.route';
+import { getAndParseExcelEmail } from '../../common-modules/server/utils/email';
 
 const router = genericRoute(attReportCtrl, router => {
     router.route('/get-edit-data')
         .get((req, res) => {
             attReportCtrl.getEditData(req, res);
+        });
+
+    router.route('/handleEmail')
+        .get(async (req, res) => {
+            const data = await getAndParseExcelEmail(req, res);
+            // attReportCtrl.handleEmail(data);
         });
 });
 
